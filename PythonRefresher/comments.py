3812363113adb31746531db8ab8eace6d99ed53a
chandru-md <chandrumd2071@gmail.com>
2026-03-14 18:06:39 +0530

    first commit of this repo and with python- variables and comments

---
+++ PythonRefresher/comments.py
@@ -0,0 +1,14 @@
+# going to print my name
+
+print("Chandru Deivanayagan")
+# GOING TO PRINT PYTHON PROGRAMMING
+
+print("python programming!!!")
+
+
+"""
+this is a fast api course i enrolled and I am revising the concepts of python which called as python refresher
+Multi line comments denoted by triple quotes or even say doc-strings.
+single line comment denoted by # (hashtag).
+
+"""
---
+++ PythonRefresher/python_basics_variable.py
@@ -0,0 +1,44 @@
+# print("Hello Chandru!!!")
+
+# variables
+
+# print(10)
+# print(3.14)
+
+cost = 100
+# print("Cost:",cost)
+
+tax_percent = 0.25
+tax = cost * tax_percent
+price = cost + tax
+
+print(price)
+
+first_name  = "Chandru"
+last_name = "Deivanayagan"
+full_name = first_name+" "+last_name
+
+print(full_name)
+
+first_name  = "Chandru"
+middle_name = "Deivanayagan"
+last_name = "Mayavan"
+full_name = first_name+" "+middle_name+" "+last_name
+
+print(full_name)
+
+first_num = 10
+second_num = 20
+print(first_num)
+print(second_num)
+
+first_num =100
+second_num = 20
+print(first_num)
+print(second_num)
+
+name = 'Chandru'
+print(name)
+
+name = "Sweety"
+print(name)
