cf22e2e93876f7b684ddcbec99788999053843db
chandru-md <chandrumd2071@gmail.com>
2026-03-14 18:15:42 +0530

    assignment-variable-exercise

--- PythonRefresher/assignment_exercise.py
+++ PythonRefresher/assignment_exercise.py
@@ -2,5 +2,5 @@ total_amount = 50
 tax_amount = 0.03
 purchasing_price = 15
 tax = purchasing_price * tax_amount
-remaining_amount = total_amount - tax
+remaining_amount = total_amount - purchasing_price - tax
 print("Remaining amount: ",remaining_amount)
