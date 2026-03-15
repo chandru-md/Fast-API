a74b0600e6519bd7df565a5f54123ece242cffa2
chandru-md <chandrumd2071@gmail.com>
2026-03-15 16:33:24 +0530

    list and list-comprehension

--- PythonRefresher/list.py
+++ PythonRefresher/list.py
@@ -2,4 +2,38 @@
 
 list are collection of data
 """
+from sklearn.inspection import permutation_importance
 
+my_list = [1,2,3,4,5,6,7,8,9,10]
+print(my_list)
+
+people_list = ["Chandru","lokesh","Kadhirvel","Boominath"]
+print(people_list)
+
+print(my_list[-1])
+
+print(my_list[1])
+
+print(people_list[1])
+
+print(people_list[-1])
+
+print(people_list[1:2])
+
+print(my_list[1:7])
+
+print(my_list)
+my_list.append(1000)
+print(my_list)
+
+my_list.insert(0,0)
+print(my_list)
+
+my_list.remove(10)
+print(my_list)
+
+my_list.pop()
+print(my_list)
+
+my_list.sort()
+print(my_list)
