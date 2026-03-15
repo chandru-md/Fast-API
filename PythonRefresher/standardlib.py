3704500e90d3c5fb9b61a95568e6c9a6a7bc04c7
chandru-md <chandrumd2071@gmail.com>
2026-03-15 22:39:23 +0530

    functions -continued by  imports in python.

---
+++ PythonRefresher/standardlib.py
@@ -0,0 +1,24 @@
+"""
+from sqlalchemy.util import methods_equivalent
+
+standard lirary comes with python,
+Use ful methods
+"""
+
+# random
+
+import random
+
+types_of_drinks = ['soda','water','coffee','Tea']
+print(random.choice(types_of_drinks))
+
+
+print(random.randint(1,5))
+
+import math
+
+square_root = math.sqrt(100)
+print(square_root)
+
+cube_root = math.cbrt(1000)
+print(cube_root)
