36ae19776ab4bbe49d28e9175b5da7453c0a0c6a
chandru-md <chandrumd2071@gmail.com>
2026-03-30 22:52:33 +0530

    OOPs in python

---
+++ PythonRefresher/OOp.py
@@ -0,0 +1,28 @@
+"""
+OOP
+It is a programming paradigm based on the concepts of objects, which can contain data and code
+OOP has benefits that include,
+Scalability
+Efficiency
+Reusability
+
+
+What are Objects
+object can define by behaviour and state
+
+"""
+
+class dog:
+    legs: int = 4
+    ears: int = 2
+    type: str = "Goofy"
+    age: int = 5
+    color: str = "Yellow"
+
+
+"""
+1. Encapsulation
+2. Abstraction
+3. Inheritance
+4. Polymorphism
+"""
